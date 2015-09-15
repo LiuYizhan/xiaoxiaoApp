730ada4cbfd96ccd6e9c18f4d6fd9b99ed7f9966
刘毅展 <SHANDIAN613@liuyitentekiMacBook-Pro.local>
2015-09-15 22:48:58 +0800

    Initial Commit

---
+++ xiaoxiaoApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xiaoxiaoApp
+//
+//  Created by 刘毅展 on 15/9/15.
+//  Copyright (c) 2015年 刘毅展. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ xiaoxiaoApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  xiaoxiaoApp
+//
+//  Created by 刘毅展 on 15/9/15.
+//  Copyright (c) 2015年 刘毅展. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
